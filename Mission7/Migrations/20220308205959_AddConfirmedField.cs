ee9b3270ae198320e8258c2deec62cbab805ffc3
jkbehling <90301783+jkbehling@users.noreply.github.com>
2022-03-16 14:10:13 -0600

    Copy Files

---
+++ Mission7/Migrations/20220308205959_AddConfirmedField.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Mission7.Migrations
+{
+    public partial class AddConfirmedField : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+           
+
+            migrationBuilder.AddColumn<bool>(
+                name: "PurchaseConfirmed",
+                table: "Purchases",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PurchaseConfirmed",
+                table: "Purchases");
+
+           
+        }
+    }
+}
---
+++ Mission7/Models/IBookstoreRepository.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission7.Models
+{
+    public interface IBookstoreRepository
+    {
+        IQueryable<Book> Books { get; }
+
+        public void SaveBook(Book b);
+        public void CreateBook(Book b);
+        public void DeleteBook(Book b);
+    }
+}
